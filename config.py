dea2aa21706831093a29016dcd6e76debed66c18
POCHOTOVVALENTYN <shuba.valentin2000@gmail.com>
2025-12-18 10:29:48 +0200

    24

--- config.py
+++ config.py
@@ -98,6 +98,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
         case_sensitive = False
+        extra = "ignore"
 
 
 # === ГЛОБАЛЬНЫЙ ЭКЗЕМПЛЯР ===
